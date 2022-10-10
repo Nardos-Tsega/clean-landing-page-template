ec658eb3a1ee4933efe77d79f2e392d4194d139f
Nardos <nardossts@gmail.com>
2022-10-10 23:59:19 +0300

    setup component tree

---
+++ src/Foods.js
@@ -0,0 +1,29 @@
+import { Fragment } from "react";
+import "./Foods.css";
+
+const Foods = (props) => {
+    return (
+        <Fragment>
+            <h4 className="foodTitle">Choose from our menu</h4>
+            <ul className="ulFoods">
+                {props.menuItems.map((item) => {
+                    return (
+                        <li key={item.id} className="liFoods">
+                            <img 
+                            className="foodImg"
+                            src={require(`./images/${item.image}`)}
+                            alt={item.name}
+                            />
+                            <div className="foodItem">
+                                <p className="foodDesc">{item.desc}</p>
+                                <p className="foodPrice">{item.price}$</p>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
+        </Fragment>
+    )
+}
+
+export default Foods
